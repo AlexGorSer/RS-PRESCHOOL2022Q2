e39978e2ceb1fc415c150644c5c63a6c88a426d7
AlexGorSer <taskekaminari@gmail.com>
2022-07-14 15:01:32 +0200

    fix: fix bug burger

--- travel/index.js
+++ travel/index.js
@@ -1,6 +1,6 @@
 const reviu = "Самооценка в консоль:\n 1. Вёрстка валидная +10 \n 2. Вёрстка семантическая +20 \n 3. Вёрстка соответствует макету +48\n 4. Требования к css + 12\n 5. Интерактивность, реализуемая через css +20 ВНИМАНИЕ!!! Плавная прокрутка работает только в Mozilla, черт его знает почему в хроме не работает, но свойство прописано в теге <html>. \n Оценка:120\\100";
 
-alert('Приет, я не успеваю, не могли бы вы проверить перед дедлайном? Заранее спасибо!')
+// alert('Приет, я не успеваю, не могли бы вы проверить перед дедлайном? Заранее спасибо!')
 console.log(reviu);
 
 const burgerButton = document.querySelector('.burger__button'),
@@ -20,4 +20,11 @@ burgerButton.addEventListener('click', () => {
 burgerCloseButton.addEventListener('click', () => {
     burgerMenu.classList.remove('burger__menu-active');
     burgerConteiner.classList.remove('burger__menu-active');
+});
+
+burgerConteiner.addEventListener('click', (e) => {
+    if (e.target === burgerConteiner || e.target === e.target.closest('.navigation__menu') || e.target.closest('.burger__menu-active')) {
+        burgerMenu.classList.remove('burger__menu-active');
+        burgerConteiner.classList.remove('burger__menu-active');
+    }
 })
